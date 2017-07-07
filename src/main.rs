d4cf507327b74df4906c330bd5f869dd2030be71
Will Greenberg <ifnspifn@gmail.com>
2017-07-06 22:13:48 -0700

    correct sobel

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ extern crate gst;
 extern crate rscam;
 
 use rscam::{Camera, Config, Frame};
-const HEIGHT: usize = 480;
-const WIDTH: usize = 640;
-const BUF_SIZE: usize = HEIGHT * WIDTH * 2;
+const HEIGHT: u32 = 480;
+const WIDTH: u32 = 640;
+const BUF_SIZE: usize = (HEIGHT * WIDTH) as usize * 2;
 
 struct Neighbors<'a> {
     frame: &'a Frame,
     idx: i64,
 }
 impl<'a> Iterator for Neighbors<'a> {
-    type Item = [u8; 9];
-    fn next (&mut self) -> Option<[u8; 9]> {
+    type Item = [i64; 9];
+    fn next (&mut self) -> Option<[i64; 9]> {
         if self.idx == (HEIGHT * WIDTH) as i64 {
             None
         } else {
@@ -23,13 +23,13 @@ impl<'a> Iterator for Neighbors<'a> {
             self.idx += 1;
             Some([
                 get_pix((x-1, y-1), self.frame),
-                get_pix((x, y-1), self.frame),
+                get_pix((x  , y-1), self.frame),
                 get_pix((x+1, y-1), self.frame),
-                get_pix((x-1, y), self.frame),
-                get_pix((x, y), self.frame),
-                get_pix((x+1, y), self.frame),
+                get_pix((x-1, y  ), self.frame),
+                get_pix((x  , y  ), self.frame),
+                get_pix((x+1, y  ), self.frame),
                 get_pix((x-1, y+1), self.frame),
-                get_pix((x, y+1), self.frame),
+                get_pix((x  , y+1), self.frame),
                 get_pix((x+1, y+1), self.frame),
             ])
         }
@@ -40,21 +40,28 @@ fn convolve (frame: & Frame) -> Neighbors {
     Neighbors { frame: frame, idx: 0 }
 }
 
-fn get_pix ((x, y): (i64, i64), frame: &Frame) -> u8 {
+fn get_pix ((x, y): (i64, i64), frame: &Frame) -> i64 {
     if x < 0 || y < 0 || x >= WIDTH as i64 || y >= HEIGHT as i64 {
         0
     } else {
-        let idx = ((x + y * (WIDTH as i64)) / 2) as usize;
-        let y0 = frame[idx];
-        let y1 = frame[idx + 2];
-        if x % 2 == 0 { y0 } else { y1 }
+        let idx = ((x + y * (WIDTH as i64)) * 2) as usize;
+        frame[idx] as i64
     }
 }
 
-fn filter (input: &Frame, output: &mut [u8]) {
+fn sobel (input: &Frame, output: &mut [u8]) {
     let mut i = 0;
     for neighbors in convolve(input) {
-        output[i+0] = neighbors[4];
+        let mut dx = neighbors[0] + neighbors[6];
+        dx += neighbors[3] + neighbors[3];
+        dx -= neighbors[5] + neighbors[5];
+        dx -= neighbors[2] + neighbors[8];
+        let mut dy = neighbors[0] + neighbors[2];
+        dy += neighbors[1] + neighbors[1];
+        dy -= neighbors[7] + neighbors[7];
+        dy -= neighbors[6] + neighbors[8];
+        let pix = ((dx * dx + dy * dy) as f64).sqrt();
+        output[i+0] = pix as u8;
         output[i+1] = 0x80;
         i += 2;
     }
@@ -63,14 +70,15 @@ fn filter (input: &Frame, output: &mut [u8]) {
 fn main() {
     // GStreamer setup
     gst::init();
-    let mut pipeline = gst::Pipeline::new_from_str("appsrc caps=\"video/x-raw,format=YUY2,width=640,height=480,framerate=1/30\" name=appsrc0 ! videoconvert ! autovideosink").unwrap();
+    let str = format!("appsrc caps=\"video/x-raw,format=YUY2,width={},height={},framerate=1/30\" name=appsrc0 ! videoconvert ! autovideosink", WIDTH, HEIGHT);
+    let mut pipeline = gst::Pipeline::new_from_str(str.as_str()).unwrap();
     let mut mainloop = gst::MainLoop::new();
 
     // rscam setup
     let mut camera = Camera::new("/dev/video0").unwrap();
     camera.start(&Config {
         interval: (1, 30),
-        resolution: (640, 480),
+        resolution: (WIDTH, HEIGHT),
         ..Default::default()
     }).unwrap();
 
@@ -78,7 +86,7 @@ fn main() {
     let mut appsrc = gst::AppSrc::new_from_element(appsrc);
     let mut bufferpool = gst::BufferPool::new().unwrap();
     let appsrc_caps = appsrc.caps().unwrap();
-    bufferpool.set_params(&appsrc_caps,640*480*2,0,0);
+    bufferpool.set_params(&appsrc_caps,WIDTH*HEIGHT*2 as u32,0,0);
     if bufferpool.set_active(true).is_err(){
         panic!("Couldn't activate buffer pool");
     }
@@ -97,12 +105,12 @@ fn main() {
             if frame.len() != BUF_SIZE {
                 continue;
             }
-            filter(&frame, &mut cv_buf);
+            sobel(&frame, &mut cv_buf);
             if let Some(mut buffer) = bufferpool.acquire_buffer() {
                 let mut i = 0;
                 // copy the webcam frame to the appsrc... is there a better way
                 // to do this???
-                buffer.map_write(|mut mapping| {
+                buffer.map_write(|mapping| {
                     for c in mapping.iter_mut::<u8>() {
                         *c = cv_buf[i];
                         i += 1;
